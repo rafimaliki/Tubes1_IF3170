083aae94fb2d11eceb1ec919c88382af6573282c
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-10-25 00:03:44 +0700

    simplify router

--- src/be/routes/routes.go
+++ src/be/routes/routes.go
@@ -8,21 +8,15 @@ import (
 
 func InitRoutes(router *gin.Engine) {
     routes := []struct {
-        method  string
         path    string
         handler gin.HandlerFunc
     }{
-		{"GET", "/hill-climbing/:cube", controllers.HillClimbing},
-		{"GET", "/simulated-annealing/:cube", controllers.SimulatedAnnealing},
-		{"GET", "/genetic-algorithm/:cube", controllers.GeneticAlgorithm},
+		{"/hill-climbing/:cube", controllers.HillClimbing},
+		{"/simulated-annealing/:cube", controllers.SimulatedAnnealing},
+		{"/genetic-algorithm/:cube", controllers.GeneticAlgorithm},
     }
 
     for _, route := range routes {
-        switch route.method {
-        case "GET":
-            router.GET(route.path, route.handler)
-        case "POST":
-            router.POST(route.path, route.handler)
-        }
+        router.GET(route.path, route.handler)
     }
 }
