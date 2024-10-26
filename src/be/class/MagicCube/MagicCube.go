9d6c7428c05870ee0220c031c8305fc65f637066
Nicholas Sihite <nicholassihite05@gmail.com>
2024-10-26 14:47:12 +0700

    add: objective function

--- src/be/class/MagicCube/MagicCube.go
+++ src/be/class/MagicCube/MagicCube.go
@@ -6,40 +6,40 @@ import (
 )
 
 type MagicCube struct {
-    Size   int
-    Buffer [][][]int
+	Size   int
+	Buffer [][][]int
 }
 
 func New(size ...int) *MagicCube {
-    var n int
-    if len(size) > 0 {
-        n = size[0] 
-    } else {
-        n = 5   
-    }
+	var n int
+	if len(size) > 0 {
+		n = size[0]
+	} else {
+		n = 5
+	}
 
-    buffer := make([][][]int, n)
-    for i := range buffer {
-        buffer[i] = make([][]int, n)
-        for j := range buffer[i] {
-            buffer[i][j] = make([]int, n)
-        }
-    }
+	buffer := make([][][]int, n)
+	for i := range buffer {
+		buffer[i] = make([][]int, n)
+		for j := range buffer[i] {
+			buffer[i][j] = make([]int, n)
+		}
+	}
 
-    return &MagicCube{
-        Size:   n,
-        Buffer: buffer,
-    }
+	return &MagicCube{
+		Size:   n,
+		Buffer: buffer,
+	}
 }
 
 func (mc *MagicCube) Print() {
-    for i := 0; i < mc.Size; i++ {
-        fmt.Printf("Layer %d:\n", i+1)
-        for j := 0; j < mc.Size; j++ {
-            fmt.Println(mc.Buffer[i][j])
-        }
-        fmt.Println() 
-    }
+	for i := 0; i < mc.Size; i++ {
+		fmt.Printf("Layer %d:\n", i+1)
+		for j := 0; j < mc.Size; j++ {
+			fmt.Println(mc.Buffer[i][j])
+		}
+		fmt.Println()
+	}
 }
 
 func (mc *MagicCube) Shuffle() {
@@ -60,4 +60,106 @@ func (mc *MagicCube) Shuffle() {
 			}
 		}
 	}
+}
+
+func absoluteVal(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
+}
+
+func (mc *MagicCube) objectiveFunction() int {
+	dimension := mc.Size
+	score := 0
+	magicNumber := dimension * (dimension*dimension*dimension + 1) / 2
+
+	for y := 0; y < dimension; y++ {
+		for z := 0; z < dimension; z++ {
+			rowSum := 0
+			for x := 0; x < dimension; x++ {
+				rowSum += mc.Buffer[x][y][z]
+			}
+			score += absoluteVal(rowSum - magicNumber)
+		}
+	}
+
+	for x := 0; x < dimension; x++ {
+		for y := 0; y < dimension; y++ {
+			columnSum := 0
+			for z := 0; z < dimension; z++ {
+				columnSum += mc.Buffer[x][y][z]
+			}
+			score += absoluteVal(columnSum - magicNumber)
+		}
+	}
+
+	for x := 0; x < dimension; x++ {
+		for z := 0; z < dimension; z++ {
+			pillarSum := 0
+			for y := 0; y < dimension; y++ {
+				pillarSum += mc.Buffer[x][y][z]
+			}
+			score += absoluteVal(pillarSum - magicNumber)
+		}
+	}
+
+	for z := 0; z < dimension; z++ {
+		diag1XY := 0
+		diag2XY := 0
+
+		for i := 0; i < dimension; i++ {
+			diag1XY += mc.Buffer[i][i][z]
+			diag2XY += mc.Buffer[i][dimension-i-1][z]
+		}
+
+		score += absoluteVal(diag1XY - magicNumber)
+		score += absoluteVal(diag2XY - magicNumber)
+	}
+
+	for y := 0; y < dimension; y++ {
+		diag1XZ := 0
+		diag2XZ := 0
+
+		for i := 0; i < dimension; i++ {
+			diag1XZ += mc.Buffer[i][y][i]
+			diag2XZ += mc.Buffer[i][y][dimension-i-1]
+		}
+
+		score += absoluteVal(diag1XZ - magicNumber)
+		score += absoluteVal(diag2XZ - magicNumber)
+	}
+
+	for x := 0; x < dimension; x++ {
+		diag1YZ := 0
+		diag2YZ := 0
+
+		for i := 0; i < dimension; i++ {
+			diag1YZ += mc.Buffer[x][i][i]
+			diag2YZ += mc.Buffer[x][i][dimension-i-1]
+		}
+
+		score += absoluteVal(diag1YZ - magicNumber)
+		score += absoluteVal(diag2YZ - magicNumber)
+	}
+
+	mainDiag3D := 0
+	antiDiag1_3D := 0
+	antiDiag2_3D := 0
+	antiDiag3_3D := 0
+
+	for i := 0; i < dimension; i++ {
+		mainDiag3D += mc.Buffer[i][i][i]
+		antiDiag1_3D += mc.Buffer[i][dimension-i-1][i]
+		antiDiag2_3D += mc.Buffer[dimension-i-1][i][i]
+		antiDiag3_3D += mc.Buffer[dimension-i-1][dimension-i-1][i]
+	}
+
+	score += absoluteVal(mainDiag3D - magicNumber)
+	score += absoluteVal(antiDiag1_3D - magicNumber)
+	score += absoluteVal(antiDiag2_3D - magicNumber)
+	score += absoluteVal(antiDiag3_3D - magicNumber)
+
+	return -score
 }
