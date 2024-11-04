b9c35f19a5fc2e4df940c37164c4f5f9574f1bf7
Nicholas Sihite <nicholassihite05@gmail.com>
2024-11-04 09:41:51 +0700

    fix: specifications requirements

--- src/be/algorithm/simulatedAnnealing.go
+++ src/be/algorithm/simulatedAnnealing.go
@@ -10,6 +10,9 @@ type Response struct {
 	Buffer             [][][]int
 	IndexChange        [][][]int
 	ObjectiveFunctions []int
+	LocalOptimum       int
+	Iterations         int
+	DeltaE             []int
 }
 
 func SimulatedAnnealing(cube *MagicCube.MagicCube) (Response, error) {
@@ -21,13 +24,15 @@ func SimulatedAnnealing(cube *MagicCube.MagicCube) (Response, error) {
 	// sameOrWorseScore := 0
 	indexChange := [][][]int{}
 	objectiveFunctions := []int{}
+	objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
+	countLocalOptimum := 0
+	deltaEValues := []int{}
+	iterations := 0
 
 	for {
+		iterations++
 
 		if temperature <= 0 {
-			// print objective function
-			// fmt.Println("Same or worse score: ", sameOrWorseScore)
-			// fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
 			fmt.Println("\033[32mDone!\033[0m")
 			break
 		}
@@ -46,54 +51,45 @@ func SimulatedAnnealing(cube *MagicCube.MagicCube) (Response, error) {
 		newCube.SwapValues(swapSourceIdx, swapTargetIdx)
 
 		if newCube.ObjectiveFunction() == 0 {
-			// fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
 			fmt.Println("\033[32mDone!\033[0m")
 			bestCube = newCube
 			objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
-
-			// return newCube.Buffer, nil
 			break
 		}
 
-		// if bestCube.ObjectiveFunction() == 0 {
-		// 	fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
-		// 	fmt.Println("\033[32mDone!\033[0m")
-		// 	return bestCube.Buffer, nil
-		// }
-
 		deltaE := newCube.ObjectiveFunction() - bestCube.ObjectiveFunction()
-		// if deltaE >= 0 {
-		// 	sameOrWorseScore++
-		// }
-		// fmt.Println("Delta E: ", deltaE)
 
 		if deltaE < 0 {
+			deltaEValues = append(deltaEValues, 1)
 			bestCube = newCube
 			indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
-			objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
 		} else {
+			countLocalOptimum++
+			deltaEValues = append(deltaEValues, deltaE)
 			probability := math.Exp(-float64(deltaE) / temperature)
-			// fmt.Println("Probability: ", probability)
-			// randomNum := rand.Float64()
-			// fmt.Println("Random Number: ", randomNum)
 			goDown := probability > 0.5
 
 			if goDown {
 				indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
-				objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
 				fmt.Println("Go Down")
 				fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
 				bestCube = newCube
+			} else {
+				indexChange = append(indexChange, [][]int{{0, 0, 0}, {0, 0, 0}})
 			}
 		}
 
-		temperature -= 0.01
+		objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
+		temperature -= 0.001
 	}
 
 	Result := Response{
 		Buffer:             bestCube.Buffer,
 		IndexChange:        indexChange,
 		ObjectiveFunctions: objectiveFunctions,
+		LocalOptimum:       countLocalOptimum,
+		Iterations:         iterations,
+		DeltaE:             deltaEValues,
 	}
 
 	return Result, nil
