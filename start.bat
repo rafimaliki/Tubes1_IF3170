7986b360e936033b0a34e3b243b3c54f54c6963d
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-11-04 23:06:17 +0700

    modify bat file

--- start.bat
+++ start.bat
@@ -1,5 +1,8 @@
 @echo off
 
-start cmd /k "cd /d %~dp0 && cd src/fe && npm run dev"
+start cmd /k "cd /d %~dp0 && cd src/fe && npm i && npm run dev"
 
 start cmd /k "cd /d %~dp0 && cd src/be && go run main.go"
+
+rem 
+start "" "http://localhost:5173"
