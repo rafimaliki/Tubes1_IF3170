961cd8e2963348eaf4d5a0e05455e4825d14f86e
Nicholas Sihite <nicholassihite05@gmail.com>
2024-11-04 14:55:41 +0700

    feat: condition initial state is already magic cube

--- src/be/algorithm/simulatedAnnealing.go
+++ src/be/algorithm/simulatedAnnealing.go
@@ -15,65 +15,68 @@ func SimulatedAnnealing(cube *MagicCube.MagicCube) (MagicCube.Response, error) {
 	temperature := 100.0
 	indexChange := [][][]int{}
 	objectiveFunctions := []int{}
-	objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
 	countLocalOptimum := 0
 	deltaEValues := []int{}
 	iterations := 0
 
 	start := time.Now()
+	initialScore := bestCube.ObjectiveFunction()
+	objectiveFunctions = append(objectiveFunctions, initialScore)
 
-	for {
-		iterations++
-
-		if temperature <= 0 {
-			fmt.Println("\033[32mDone!\033[0m")
-			break
-		}
-
-		swapSourceIdx := bestCube.GetRandomIdx()
-		swapTargetIdx := bestCube.GetRandomIdx()
-
+	if initialScore != 0 {
 		for {
-			if swapSourceIdx != swapTargetIdx {
+			iterations++
+
+			if temperature <= 0 {
+				fmt.Println("\033[32mDone!\033[0m")
 				break
 			}
-			swapTargetIdx = bestCube.GetRandomIdx()
-		}
 
-		newCube := bestCube.Copy()
-		newCube.SwapValues(swapSourceIdx, swapTargetIdx)
+			swapSourceIdx := bestCube.GetRandomIdx()
+			swapTargetIdx := bestCube.GetRandomIdx()
 
-		if newCube.ObjectiveFunction() == 0 {
-			fmt.Println("\033[32mDone!\033[0m")
-			bestCube = newCube
-			objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
-			break
-		}
-
-		deltaE := newCube.ObjectiveFunction() - bestCube.ObjectiveFunction()
-
-		if deltaE > 0 {
-			deltaEValues = append(deltaEValues, 1)
-			bestCube = newCube
-			indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
-		} else {
-			countLocalOptimum++
-			deltaEValues = append(deltaEValues, deltaE)
-			probability := math.Exp(float64(deltaE) / temperature)
-			goDown := probability > 0.9
-
-			if goDown {
-				indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
-				fmt.Println("Go Down")
-				fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
-				bestCube = newCube
-			} else {
-				indexChange = append(indexChange, [][]int{{0, 0, 0}, {0, 0, 0}})
+			for {
+				if swapSourceIdx != swapTargetIdx {
+					break
+				}
+				swapTargetIdx = bestCube.GetRandomIdx()
 			}
-		}
 
-		objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
-		temperature -= 0.001
+			newCube := bestCube.Copy()
+			newCube.SwapValues(swapSourceIdx, swapTargetIdx)
+
+			if newCube.ObjectiveFunction() == 0 {
+				fmt.Println("\033[32mDone!\033[0m")
+				bestCube = newCube
+				objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
+				break
+			}
+
+			deltaE := newCube.ObjectiveFunction() - bestCube.ObjectiveFunction()
+
+			if deltaE > 0 {
+				deltaEValues = append(deltaEValues, 1)
+				bestCube = newCube
+				indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
+			} else {
+				countLocalOptimum++
+				deltaEValues = append(deltaEValues, deltaE)
+				probability := math.Exp(float64(deltaE) / temperature)
+				goDown := probability > 0.9
+
+				if goDown {
+					indexChange = append(indexChange, [][]int{swapSourceIdx[:], swapTargetIdx[:]})
+					fmt.Println("Go Down")
+					fmt.Println("Objective Function: ", bestCube.ObjectiveFunction())
+					bestCube = newCube
+				} else {
+					indexChange = append(indexChange, [][]int{{0, 0, 0}, {0, 0, 0}})
+				}
+			}
+
+			objectiveFunctions = append(objectiveFunctions, bestCube.ObjectiveFunction())
+			temperature -= 0.001
+		}
 	}
 
 	elapsed := time.Since(start)
